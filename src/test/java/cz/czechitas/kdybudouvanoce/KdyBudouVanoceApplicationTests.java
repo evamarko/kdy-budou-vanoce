c7f211dc8639364e6600ef2f9553fd0a0d4e47e6
Eva Marko <valentova.eva92@gmail.com>
2024-01-03 21:06:55 -0700

    Project started using start.spring.io

---
+++ src/test/java/cz/czechitas/kdybudouvanoce/KdyBudouVanoceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.czechitas.kdybudouvanoce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdyBudouVanoceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
